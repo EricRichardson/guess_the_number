e7d00515aa44a8f7d1c816f2308102a95cf759ed
ericrichardson <eric@blogoferic.com>
2016-02-21 20:04:55 -0500

    Input verification

--- guess.the.number.rb
+++ guess.the.number.rb
@@ -1,17 +1,41 @@
-puts "Welcome to guess the number! \n\nTry to guess the number between 1 and 10"
+puts "Welcome to Guess the Number!"
+
+again = 'y'
+
+while(again == 'y') do
+
 number = rand(5) + 1
-again = 'Y'
-while(again == 'Y') do
-guess = gets.to_i
-if guess == number 
-	puts "You guessed correctly! Congratulations! \nPlay again? (Y/N)"
-	again = gets.chomp
-	number = rand(5) + 1
-	puts "Number as be re-randomized \nGuess the number between 1 and 10"
-else
-	puts "Guess again"
-end
+guess = nil
+
+		while(number != guess.to_i) do
+		
+		puts "Guess a number between 1 and 10"
+		guess = gets.chomp
+		check = (guess.to_i.to_s == guess)
+			while (!check) do
+				puts "Invalid input!"
+				guess = gets.chomp
+				check = (guess.to_i.to_s == guess)
+			end
+			
+				if guess.to_i == number 
+					puts "You guessed correctly! Congratulations!"
+				else
+					puts "Incorrect."
+				end
+			
+		end
+		
+	repeat = true
+	
+	while (repeat) do
+		puts "Play again? (Y/N)"
+		again = gets.chomp.downcase
+		puts "Invalid input!" unless again =='y' or again =='n'
+		repeat = false if again == 'y' or again == 'n'		
+	end
+	
 end
 
-puts "Good bye"
+puts "Thanks for playing!"
 gets
